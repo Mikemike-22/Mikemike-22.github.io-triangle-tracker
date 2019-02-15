05765f3edfce61c89b191ab6301fc065f138891e
Michael Korir <mikor22@gmail.com>
2019-02-15 14:21:51 +0300

    Add parameters for the triangles

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-if(a===b && b===c && c===a){
-  alert("ISOSCELES TRIANGLE")
-}
+function trackButton();{
+  var SideA=document.getElementById("a").id;
+  var SideB=document.getElementById("b").id;
+  var SideC=document.getElementById("c").id;
+
+  if(SideA===SideB && SideB===SideC && SideB===SideC){
+  alert(" Equilateral Triangle ");
+  }
+  else if(SideA===SideB || SideA===SideC || SideC===SideB){
+  alert("Isosceles Triangle");
+  }
+  else if((SideA+SideB)<= SideC || (SideA+SideC)<=SideB || (SideB+sideC)<=SideA) {
+  alert("Not a Triangle");
+  }
+  else{
+  alert("Scalene Triangle");
+  }
+  }
