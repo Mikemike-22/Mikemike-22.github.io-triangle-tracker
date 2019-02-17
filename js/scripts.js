9eb460e9f2578f873647050bbd5cf774c7829c23
Michael Korir <mikor22@gmail.com>
2019-02-17 22:32:36 +0300

    Add changes to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
-function trackButton(){
+function myFunction(A,B,C){
   var A=document.getElementById("a").value;
   var B=document.getElementById("b").value;
   var C=document.getElementById("c").value;
 
   if(A===B && B===C && B===C){
-document.getElementById("result").innerHTML="Equilateral Triangle"
+  document.getElementById("result").innerHTML="Equilateral Triangle"
   }
   else if(A===B || A===C || C===B){
 document.getElementById("result").innerHTML="Isosceles Triangle"
   }
-  else if((A+B)<= C || (A+C)<=B || (B+C)<=A) {
-document.getElementById("result").innerHTML="Scalene Triangle"
-  }
-  else if{(A+B)>=C || (B+C)>=A || (C+A)>=B
+  else if((A+B)<=C || (A+C)<=B || (B+C)<=A) {
 document.getElementById("result").innerHTML="Not a Triangle"
+  }
+  else{
+  alert("Scalene Triangle");
   }
   }
