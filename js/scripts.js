d0c3721fea2da22da381fb7ffaf824a10432aa53
Michael Korir <mikor22@gmail.com>
2019-02-21 15:30:42 +0300

    make changes to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
 function myFunction(A,B,C){
-  var A=document.getElementById("a").value;
-  var B=document.getElementById("b").value;
-  var C=document.getElementById("c").value;
+  var A=parseInt(document.getElementById("a").value);
+  var B=parseInt(document.getElementById("b").value);
+  var C=parseInt(document.getElementById("c").value);
 
-  if(A===B && B===C && B===C){
-  document.getElementById("result").innerHTML="Equilateral Triangle"
+
+  if (A+B>C && A+C>B && B+C>A) {
+    if (A===B && B===C && B===C){
+      document.getElementById("result").innerHTML="Equilateral Triangle"
+    }else if(A===B&&B!==C || A===C && C!==B || C===B && B!==A){
+      document.getElementById("result").innerHTML="Isosceles Triangle"
+    }else{
+      document.getElementById("result").innerHTML="Scalene Triangle"
+    }
+  }else {
+    document.getElementById("result").innerHTML="Not a Triangle";
   }
-  else if(A===B || A===C || C===B){
-document.getElementById("result").innerHTML="Isosceles Triangle"
-  }
-  else if((A+B)<=C || (A+C)<=B || (B+C)<=A) {
-document.getElementById("result").innerHTML="Not a Triangle"
-  }
-  else{
-  alert("Scalene Triangle");
-  }
-  }
+
+}
